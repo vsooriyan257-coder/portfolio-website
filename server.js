d45da21366d0997fb166fdad1274b38abbd2385d
vsooriyan257-coder <v.sooriyan257@gmail.com>
2026-03-23 17:13:00 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Connect to MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/portfolioDB')
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+// Schema
+const contactSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    message: String
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+// Form route
+app.post('/submit', async (req, res) => {
+    const { name, email, message } = req.body;
+
+    const newContact = new Contact({
+        name,
+        email,
+        message
+    });
+
+    await newContact.save();
+
+    res.send("Message saved successfully!");
+});
+
+// Start server
+app.listen(3000, () => {
+    console.log("Server running at http://localhost:3000");
+});
